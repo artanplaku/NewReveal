6edfcdd7c90c3423736afeee44c1c333f28e6a52
Artan <artanpl03@gmail.com>
2022-12-14 18:36:57 -0500

    created categories and added sass

--- newreveal/src/App.js
+++ newreveal/src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import './categoriesStyles.scss'
 
 function App() {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    }
+  ]
+
+
   return (
-    <div className="App">
-      Hello
-    </div>
+      <div className='categoriesContainer'>
+        {categories.map(({title})=>(
+          <div className='categoryContainer'>
+          <div className='backgroundImg'/>
+          <div className='categoryBodyContainer'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+      </div>
+        ))}
+        
+      </div>
   );
 }
 
