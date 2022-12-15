c03dafcbcc0d32588ee1bcac22a27606d3a675e3
Artan <artanpl03@gmail.com>
2022-12-15 12:37:05 -0500

    created CategoryList component

--- newreveal/src/App.js
+++ newreveal/src/App.js
@@ -1,46 +1,14 @@
 import './App.css';
 import './categoriesStyles.scss'
-import CategoryItem from './components/categoryItem/CategoryItem';
+import CategoryList from './components/CategoryList/CategoryList';
 
 function App() {
 
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    }
-  ]
-
-
-  return (
-      <div className='categoriesContainer'>
-        {categories.map((category)=>(
-          <CategoryItem key={category.id} category={category} />
-        ))}
-        
-      </div>
-  );
+  return(
+    <div>
+      <CategoryList />
+    </div>
+  )
 }
 
 export default App;
