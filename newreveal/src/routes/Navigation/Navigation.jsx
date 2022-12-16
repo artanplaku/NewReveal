0dc15f4279cf10b764cf80b94feb3ae139efc4ae
Artan <artanpl03@gmail.com>
2022-12-15 21:11:21 -0500

    added logo and small scss changes

--- newreveal/src/routes/Navigation/Navigation.jsx
+++ newreveal/src/routes/Navigation/Navigation.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Fragment } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import './Navigation.scss'
+
 const Navigation = () => {
     return(
 // Fragment acts like a parent div without adding anything to the DOM - no extra divs ----------------------------
         <Fragment>
           <div className='navigation'>
             <Link className='logoContainer' to='/'>
-            <div>Logo</div>
+            <CrwnLogo className='logo' />
             </Link>
             <div className='linksContainer'>
                <Link className='navLink' to='/shop'>
